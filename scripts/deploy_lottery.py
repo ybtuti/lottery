d451a88ec109df2c5fbefc35d0cd394386489ba4
0xodus <samuelmsuu@gmail.com>
2024-05-15 14:51:56 +0300

    added deployment files and some tests

---
+++ scripts/deploy_lottery.py
@@ -0,0 +1,9 @@
+from scripts.helpful_scripts import get_account, get_contract
+from brownie import Lottery
+
+def deploy_lottery():
+    account = get_account()
+    lottery = Lottery.deploy(get_contract("eth_usd_price_feed"))
+
+def main():
+    deploy_lottery()
